523f2bd0b857d2be63321812053857e305a27084
github_emnamhi123 <emna-mhiri@hotmail.com>
2024-03-08 20:13:36 +0100

    create signin api route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-// import  from 'react'
 import Home from "./pages/Home";
 import About from './pages/About';
 import SignIn from './pages/SignIn';
@@ -7,19 +6,21 @@ import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
 import SignUp from './pages/SignUp'; 
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <BrowserRouter>
     <Header/>
     <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/about" element={<About />} />
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/projects" element={<Projects />} />
+      <Route path='/' element={<Home />}/>
+      <Route path='/about' element={<About />} />
+      <Route path='/sign-in' element={<SignIn />} />
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/projects' element={<Projects />} />
     </Routes>
+    <Footer/>
     </BrowserRouter>
-  )
+  );
 }
